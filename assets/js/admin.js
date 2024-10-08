3a127093fe23ea7eec535c38d5960d7df54e283c
“Gökdeniz <gokdenizkeeles@gmail.com>
2024-10-08 16:00:34 +0300

    admin

---
+++ assets/js/admin.js
@@ -0,0 +1,8 @@
+let kullaniciAdi = prompt("Kullanıcı adınızı giriniz")
+let sifre = (Number) (prompt("şifrenizi giriniz"))
+if (kullaniciAdi === "admin"  && sifre === 1234) {
+ alert("Başarılı giriş")
+ }  else {
+  alert ("şifre veya kullanıcı adı yanlış")
+ }
+ 
